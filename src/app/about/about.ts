97d3c3d9ecdbc82a84f5285173046e31adf8ca51
Anton Moiseev <a@antonmoiseev.com>
2016-03-07 16:48:16 +0300

    Inital commit

---
+++ src/app/about/about.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'my-about',
+  styles: [require('./about.css')],
+  template: require('./about.html'),
+})
+export class About {}
